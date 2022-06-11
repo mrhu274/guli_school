54496da19b37ec377efcbb106d1a24ff832f8069
hhh <2213916262@qq.com>
2022-06-11 13:30:33 +0800

    阿里云上传。easyExecl，树形结构展示

--- service/service_edu/src/main/java/com/itguigu/eduservice/service/EduTeacherService.java
+++ service/service_edu/src/main/java/com/itguigu/eduservice/service/EduTeacherService.java
@@ -1,7 +1,10 @@
 package com.itguigu.eduservice.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itguigu.eduservice.entity.EduTeacher;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.itguigu.eduservice.query.TeacherQuery;
 
 /**
  * <p>
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-06-07
  */
 public interface EduTeacherService extends IService<EduTeacher> {
-
+    IPage<EduTeacher> pageQuery(Page<EduTeacher> pageParam, TeacherQuery teacherQuery);
 }
