4eb0235957faa2613a006eb70c9a91dae53a8579
hhh <2213916262@qq.com>
2022-06-11 13:29:44 +0800

    阿里云上传。easyExecl，树形结构展示

--- service/service_edu/src/main/java/com/itguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/itguigu/eduservice/controller/EduTeacherController.java
@@ -1,14 +1,18 @@
 package com.itguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itguigu.eduservice.entity.EduTeacher;
+import com.itguigu.eduservice.query.TeacherQuery;
 import com.itguigu.eduservice.service.EduTeacherService;
+import com.itguigu.servicebase.exceptionhandler.GuliException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,22 +24,94 @@ import java.util.List;
  * @author testjava
  * @since 2022-06-07
  */
+@Api(tags = "讲师管理")
 @RestController
+@CrossOrigin
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
     @Autowired
     private EduTeacherService eduTeacherService;
 
-   //查询所有讲师
+    /**
+     * 查询所有讲师
+     */
+    @ApiOperation(value = "查询所有讲师")
     @GetMapping("getAllTeacher")
-    public List<EduTeacher> getAllTeacher() {
-        return eduTeacherService.list(null);
+    public R getAllTeacher() {
+        try {
+            int i = 1 / 0;
+        } catch (Exception e) {
+
+            throw new GuliException(20001,"出现自定义异常");
+        }
+        return R.ok().data("items", eduTeacherService.list(null));
     }
-    //根据id逻辑删除讲师
-    @RequestMapping("{id}")
-    public boolean deleteTeacher(@PathVariable("id") String id) {
+
+    /**
+     * 根据id逻辑删除讲师
+     */
+    @ApiOperation(value = "逻辑删除")
+    @DeleteMapping("{id}")
+    public R deleteTeacher(@ApiParam(name = "id", value = "讲师id", required = true) @PathVariable("id") String id) {
         boolean b = eduTeacherService.removeById(id);
-        return b;
+        if (b) {
+            return R.ok();
+        }
+        return R.error().message("删除失败");
+    }
+
+    /**
+     * 分页查询讲师
+     */
+    @ApiOperation(value = "分页讲师列表")
+    @PostMapping("{page}/{limit}")
+    public R getTeacherByPage(@ApiParam(name = "page", value = "当前页", required = true) @PathVariable("page") Integer page,
+                              @ApiParam(name = "limit", value = "每页条数", required = true) @PathVariable("limit") Integer limit
+            , @ApiParam(name = "teacherQuery", value = "查询对象", required = false)
+                             @RequestBody( required = false) TeacherQuery teacherQuery
+    ) {
+
+        Page<EduTeacher> pageTeacher = new Page<>(page, limit);
+        IPage<EduTeacher> eduTeacherIPage = eduTeacherService.pageQuery(pageTeacher, teacherQuery);
+       // IPage<EduTeacher> records = eduTeacherService.page(pageTeacher, null);
+        long total = eduTeacherIPage.getTotal();
+        return R.ok().data("total", total).data("rows", eduTeacherIPage);
+    }
+
+    /**
+     * 新增讲师
+     */
+    @ApiOperation(value = "新增讲师")
+    @PostMapping
+    public R save(@ApiParam(name = "EduTeacher", value = "添加讲师", required = true) @RequestBody EduTeacher teacher) {
+        System.out.println(teacher);
+//      EduTeacher(id=null, name=90, intro=1, career=1, level=1, avatar=, sort=0, isDeleted=null, gmtCreate=null, gmtModified=null)
+        eduTeacherService.save(teacher);
+        return R.ok();
+    }
+
+    /**
+     * 根据ID查询讲师
+     */
+    @ApiOperation("查询讲师")
+    @GetMapping("{id}")
+    public R getById(@ApiParam(name = "Id", value = "讲师id", required = true) @PathVariable("id") String id) {
+        EduTeacher teacher = eduTeacherService.getById(id);
+        return R.ok().data("item", teacher);
+    }
+
+    /**
+     * 根据ID修改讲师
+     */
+    @ApiOperation(value = "根据ID修改讲师")
+    @PutMapping("{id}")
+    public R updateById(@ApiParam(name = "id", value = "讲师ID", required = true)
+                        @PathVariable String id,
+                        @ApiParam(name = "teacher", value = "讲师对象", required = true)
+                        @RequestBody EduTeacher teacher) {
+        eduTeacherService.getById(id);
+        eduTeacherService.updateById(teacher);
+        return R.ok();
     }
 }
 
--- service/service_edu/src/main/java/com/itguigu/eduservice/entity/EduTeacher.java
+++ service/service_edu/src/main/java/com/itguigu/eduservice/entity/EduTeacher.java
@@ -1,13 +1,12 @@
 package com.itguigu.eduservice.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
+
 import java.io.Serializable;
 
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,41 +25,35 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@ApiModel(value="EduTeacher对象", description="讲师")
+@ApiModel(value = "EduTeacher对象", description = "讲师")
 public class EduTeacher implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     @ApiModelProperty(value = "讲师ID")
-    @TableId(value = "id", type = IdType.ID_WORKER)
+    //@TableId(value = "id", type = IdType.ID_WORKER)
+    @TableId(value = "id", type =IdType.UUID)
     private String id;
-
     @ApiModelProperty(value = "讲师姓名")
     private String name;
-
     @ApiModelProperty(value = "讲师简介")
     private String intro;
-
     @ApiModelProperty(value = "讲师资历,一句话说明讲师")
     private String career;
-
     @ApiModelProperty(value = "头衔 1高级讲师 2首席讲师")
     private Integer level;
-
     @ApiModelProperty(value = "讲师头像")
     private String avatar;
-
     @ApiModelProperty(value = "排序")
     private Integer sort;
-
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
     @TableLogic
     private Integer isDeleted;
 
     @ApiModelProperty(value = "创建时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime gmtCreate;
-
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(value = "更新时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime gmtModified;
